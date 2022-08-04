04e4b0e4b82e7e5571c1dfee3d23068fc1c22221
privacy97 <1234dnjsrn@naver.com>
2022-08-04 21:03:08 +0900

    1st_commit

---
+++ Selenium_11st_Crawling.py
@@ -0,0 +1,98 @@
+
+# step1.관련 패키지 import
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from bs4 import BeautifulSoup
+
+
+# step2. 키워드, 업체명 리스트
+keyword_list = [
+    '빵',
+    '식빵',
+    '단팥빵',
+    '호밀빵'
+]
+
+company_list = [
+
+]
+
+
+# step3.크롬드라이버로 원하는 url로 접속
+result = None
+while result is None:
+    try:
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        url = 'https://www.11st.co.kr/main'
+        driver = webdriver.Chrome(
+            options=options, executable_path=r'C:\\Users\\wjsdnjsrn\\Desktop\\SearchM_Crawling\\Python\\chromedriver_win32\\chromedriver.exe')
+        driver.get(url)
+        driver.implicitly_wait(3)
+        result = driver
+    except:
+        pass
+
+
+# selenium 버전 출력
+print()
+print('Selenium', webdriver.__version__)
+print()
+
+
+# 키워드 입력 함수
+for i in range(len(keyword_list)):
+    result = None
+    while result is None:
+        try:
+            # 검색어 입력
+            driver.find_element('class name', 'search_text').send_keys(
+                Keys.CONTROL + "a")
+            driver.find_element(
+                'class name', 'search_text').send_keys(Keys.DELETE)
+            driver.find_element(
+                'class name', 'search_text').send_keys(keyword_list[i])
+            driver.find_element(
+                'class name', 'search_text').send_keys(Keys.ENTER)
+            driver.implicitly_wait(3)
+            result = driver
+        except:
+            continue
+
+    # 검색어 출력
+    print(i+1, end='')
+    print('.', keyword_list[i])
+
+    # 광고물(AD) 접근 함수
+    result = None
+    while result is None:
+        try:
+            # 광고틀 div 접근
+            ad_div = driver.find_element(
+                'class name', 'c_listing.c_listing_view_type_list')
+            # 상품 div 저장
+            products = ad_div.find_elements('class name', 'c_card.c_card_list')
+            driver.implicitly_wait(3)
+            result = products
+        except:
+            continue
+
+    # 상품 개수 만큼 반복
+    for i in range(len(products)):
+        result = None
+        while result is None:
+            try:
+                # 회사명 추출
+                company = products[i].find_element('class name', 'name').text
+                # 물품명 추출
+                product_name = products[i].find_element(
+                    'class name', 'c_prd_name.c_prd_name_row_1').text
+                driver.implicitly_wait(3)
+                result = product_name
+            except:
+                continue
+
+        print('   ', i+1, end='')
+        print(')', company, '-', product_name)
+    print()
---
+++ Selenium_Naver_Crawling.py
@@ -0,0 +1,130 @@
+
+# 관련 패키지 import
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from bs4 import BeautifulSoup
+
+
+# 키워드 리스트
+keyword_list = [
+    '빵',
+    '식빵',
+    '단팥빵',
+    '호밀빵'
+]
+
+# 업체명 리스트
+company_list = [
+
+]
+
+
+# 크롬드라이버로 원하는 url로 접속
+result = None
+while result is None:
+    try:
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        url = 'https://search.shopping.naver.com/search/all?query=a'
+        driver = webdriver.Chrome(
+            options=options, executable_path=r'C:\\Users\\wjsdnjsrn\\Desktop\\SearchM_Crawling\\Python\\chromedriver_win32\\chromedriver.exe')
+        driver.get(url)
+        driver.implicitly_wait(1)
+        result = driver
+    except:
+        pass
+
+
+# selenium 버전 출력
+print()
+print('Selenium', webdriver.__version__)
+print()
+
+
+# 키워드 입력
+for i in range(len(keyword_list)):
+    result = None
+    while result is None:
+        try:
+            driver.find_element('class name', 'searchInput_search_input__1Eclz').send_keys(
+                Keys.CONTROL + "a")
+            driver.find_element(
+                'class name', 'searchInput_search_input__1Eclz').send_keys(Keys.DELETE)
+            driver.find_element(
+                'class name', 'searchInput_search_input__1Eclz').send_keys(keyword_list[i])
+            driver.find_element(
+                'class name', 'searchInput_search_input__1Eclz').send_keys(Keys.ENTER)
+            driver.implicitly_wait(3)
+            result = driver
+        except:
+            continue
+
+    # 키워드 출력
+    print(i+1, end='')
+    print('.', keyword_list[i])
+
+    # 상품 접근
+    result = None
+    while result is None:
+        try:
+            # 스크롤 다운(네이버 특별)
+            driver.execute_script(
+                'window.scrollTo(0, document.body.scrollHeight);')
+            # 상품 리스트 div 접근
+            products_div = driver.find_element('class name', 'list_basis')
+            # 일반 상품 div 저장
+            Normal_products = products_div.find_elements(
+                'class name', 'basicList_item__2XT81')
+            # 광고 상품 div 저장
+            Ad_products = products_div.find_elements(
+                'class name', 'basicList_item__2XT81.ad')
+            driver.implicitly_wait(3)
+
+            if len(Ad_products) >= 1 and len(Normal_products) >= 40:
+                result = Ad_products
+        except:
+            driver.refresh()
+            continue
+
+    print('  <NOMAL>')
+
+    # 일반 상품 출력
+    for j in range(len(Normal_products)):
+        result = None
+        while result is None:
+            try:
+                # 회사명 추출
+                Normal_company = Normal_products[j].find_element(
+                    'class name', 'basicList_mall_title__3MWFY').text
+                # 물품명 추출
+                Normal_product_name = Normal_products[j].find_element(
+                    'class name', 'basicList_link__1MaTN').text
+                driver.implicitly_wait(3)
+                result = Normal_product_name
+            except:
+                continue
+        print('   ', j+1, end='')
+        print(')', Normal_company, '-', Normal_product_name)
+
+    print()
+    print('  <AD>')
+
+    # 광고 상품 출력
+    for j in range(len(Ad_products)):
+        result = None
+        while result is None:
+            try:
+                # 회사명 추출
+                Ad_company = Ad_products[j].find_element(
+                    'class name', 'basicList_mall_title__3MWFY').text
+                # 물품명 추출
+                Ad_product_name = Ad_products[j].find_element(
+                    'class name', 'basicList_link__1MaTN').text
+                driver.implicitly_wait(3)
+                result = Ad_product_name
+            except:
+                continue
+        print('   ', j+1, end='')
+        print(')', Ad_company, '-', Ad_product_name)
+    print()
