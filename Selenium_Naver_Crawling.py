4034b71e397552c4813ed0538c1a711e12b1c188
응이 <1234dnjsrn@naver.com>
2023-04-10 01:22:40 +0900

    Update Selenium_Naver_Crawling.py

--- Selenium_Naver_Crawling.py
+++ Selenium_Naver_Crawling.py
@@ -1,34 +1,24 @@
 
-# 관련 패키지 import
+# step1.관련 패키지 import
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 from bs4 import BeautifulSoup
+import pandas as pd
 
 
-# 키워드 리스트
-keyword_list = [
-    '빵',
-    '식빵',
-    '단팥빵',
-    '호밀빵'
-]
-
-# 업체명 리스트
-company_list = [
-
-]
+# step2. 키워드, 업체명 리스트
+keyword = str(input())
 
 
-# 크롬드라이버로 원하는 url로 접속
+# step3.크롬드라이버로 원하는 url로 접속
 result = None
 while result is None:
     try:
         options = webdriver.ChromeOptions()
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
         url = 'https://search.shopping.naver.com/search/all?query=a'
-        driver = webdriver.Chrome(
-            options=options, executable_path=r'C:\\Users\\wjsdnjsrn\\Desktop\\SearchM_Crawling\\Python\\chromedriver_win32\\chromedriver.exe')
+        driver = webdriver.Chrome(options=options, executable_path=r'C:\\Development\\Thesis\\main\\chromedriver_win32\\chromedriver.exe')
         driver.get(url)
         driver.implicitly_wait(1)
         result = driver
@@ -42,126 +32,115 @@ print('Selenium', webdriver.__version__)
 print()
 
 
-# 키워드 입력
-for i in range(len(keyword_list)):
-    result = None
-    while result is None:
-        try:
-            driver.find_element('class name', 'searchInput_search_input__1Eclz').send_keys(
-                Keys.CONTROL + "a")
-            driver.find_element(
-                'class name', 'searchInput_search_input__1Eclz').send_keys(Keys.DELETE)
-            driver.find_element(
-                'class name', 'searchInput_search_input__1Eclz').send_keys(keyword_list[i])
-            driver.find_element(
-                'class name', 'searchInput_search_input__1Eclz').send_keys(Keys.ENTER)
-            driver.implicitly_wait(3)
-            result = driver
-        except:
-            continue
+# 키워드 입력 함수
+result = None
+while result is None:
+    try:
+        driver.find_element('class name', 'searchInput_search_input__vLBeq').send_keys(
+            Keys.CONTROL + "a")
+        driver.find_element(
+            'class name', 'searchInput_search_input__vLBeq').send_keys(Keys.DELETE)
+        driver.find_element(
+            'class name', 'searchInput_search_input__vLBeq').send_keys(keyword)
+        driver.find_element(
+            'class name', 'searchInput_search_input__vLBeq').send_keys(Keys.ENTER)
+        driver.implicitly_wait(3)
+        result = driver
+    except:
+        continue
 
-    # 키워드 출력
-    print(i+1, end='')
-    print('.', keyword_list[i])
 
-    # 상품 접근
-    result = None
-    while result is None:
-        try:
-            # 스크롤 다운(네이버 특별)
-            driver.execute_script(
-                'window.scrollTo(0, document.body.scrollHeight);')
-            # 상품 리스트 div 접근
-            products_div = driver.find_element('class name', 'list_basis')
-            # 일반 상품 div 저장
-            Normal_products = products_div.find_elements(
-                'class name', 'basicList_item__2XT81')
-            # 광고 상품 div 저장
-            Ad_products = products_div.find_elements(
-                'class name', 'basicList_item__2XT81.ad')
-            driver.implicitly_wait(3)
+# 키워드 출력
+print(keyword)
 
-            if len(Ad_products) >= 1 and len(Normal_products) >= 40:
-                result = Ad_products
-        except:
+
+# 상품 접근 함수
+result = None
+while result is None:
+    try:
+        # 스크롤 다운(네이버 특별)
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+        # 상품 div 접근
+        products_div = driver.find_element('class name', 'list_basis')
+        # 일반상품 div
+        general_products = products_div.find_elements('class name', 'basicList_item__0T9JD')
+        # 광고상품 div
+        ad_products = products_div.find_elements('class name', 'basicList_item__0T9JD.ad')
+        driver.implicitly_wait(3)
+
+        if len(general_products) >= 40:
+            result = general_products
+        else:
             driver.refresh()
-            continue
+    except:
+        driver.refresh()
+        continue
 
-    print('  <일반상품>')
-    # 일반 상품 출력
-    for j in range(len(Normal_products)):
+
+# 상품 출력 함수
+def product_print(products):
+
+    # 상품 리스트
+    products_list = []
+
+    # 상품 개수 만큼 반복
+    for k in range(len(products)):
         result = None
         while result is None:
             try:
                 # 회사명 추출
-                Normal_company = Normal_products[j].find_element(
-                    'class name', 'basicList_mall_title__3MWFY').text
+                company = products[k].find_element('class name', 'basicList_mall_title__FDXX5').text
+                if (company[-2:] == '정보'):
+                    company = company[:-2]
+
                 # 물품명 추출
-                Normal_product_name = Normal_products[j].find_element(
-                    'class name', 'basicList_link__1MaTN').text
+                product_name = products[k].find_element('class name', 'basicList_link__JLQJf').text
                 driver.implicitly_wait(3)
-                result = Normal_product_name
+                result = product_name
             except:
                 continue
-
-        # 출력 가공
-        if Normal_company[-2::] == '정보':
-            Normal_company = Normal_company[0:-2]
-        else:
-            Normal_company = "<가격비교>"
-
-        if not Normal_company:
-            result = None
-            while result is None:
-                try:
-                    Normal_company = Normal_products[j].find_element(
-                        'css selector', 'a.basicList_mall__sbVax > img')
-                    Normal_company = Normal_company.get_attribute('alt')
-                    result = Normal_company
-                except:
-                    continue
-
-        # 출력
-        print('   ', j+1, end='')
-        print(')', Normal_company, '-', Normal_product_name)
-
-    print()
-    print('  <광고상품>')
-
-    # 광고 상품 출력
-    for j in range(len(Ad_products)):
+        
+        # 상품 카테고리 추출
         result = None
         while result is None:
             try:
-                # 회사명 추출
-                Ad_company = Ad_products[j].find_element(
-                    'class name', 'basicList_mall_title__3MWFY').text
-                # 물품명 추출
-                Ad_product_name = Ad_products[j].find_element(
-                    'class name', 'basicList_link__1MaTN').text
+                product_category = products[k].find_elements('class name', 'basicList_category__cXUaZ.basicList_nohover__TJr2_')
                 driver.implicitly_wait(3)
-                result = Ad_product_name
+                result = product_category
             except:
                 continue
 
-        # 출력 가공
-        if Ad_company[-2::] == '정보':
-            Ad_company = Ad_company[0:-2]
-        else:
-            Ad_company = "<가격비교>"
+        # 상품 카테고리 저장
+        category = ''
+        for j in range(len(product_category)):
+            category = category + product_category[j].text
+            if j != (len(product_category)-1):
+                category = category + ' > '
 
-        if not Ad_company:
-            result = None
-            while result is None:
-                try:
-                    Ad_company = Ad_products[j].find_element(
-                        'css selector', 'a.basicList_mall__sbVax > img')
-                    Ad_company = Ad_company.get_attribute('alt')
-                    result = Ad_company
-                except:
-                    continue
+        
+        # json 형식 저장
+        products_list.append(
+            {
+                "company" : company,
+                "product_name" : product_name,
+                "category" : category
+            }
+        )
+    
+    for i in products_list:
+        print(i)
 
-        # 출력
-        print('   ', j+1, end='')
-        print(')', Ad_company, '-', Ad_product_name)
-    print()
+    # 결과값 반환
+    return products_list
+
+
+
+print('AD 상품')
+print(product_print(ad_products))
+print()
+
+
+print('일반 상품')
+print(product_print(general_products))
+print()
+
